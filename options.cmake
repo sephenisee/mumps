656473b2807ea8b1fc2408e6e330a5408b919b4a
Sean Eunsik Phenisee <sephenisee@gmail.com>
2024-04-29 16:42:26 -0700

    Adding option to use preinstalled Scalapack as subdirectory prior to MUMPS

--- options.cmake
+++ options.cmake
@@ -1,5 +1,7 @@
 option(${PROJECT_NAME}_BUILD_TESTING "Build tests" ${PROJECT_IS_TOP_LEVEL})
 
+option(given_scalapack "Use provided Scalapack")
+
 option(find_static "Find static libraries for Lapack and Scalapack (default shared then static search)")
 
 if(MUMPS_UPSTREAM_VERSION VERSION_GREATER_EQUAL 5.2)
